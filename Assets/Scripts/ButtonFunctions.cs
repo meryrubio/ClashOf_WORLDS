e8284668ec03538b62c90bde9a8336a47ba67693
meryrubio <mariarg9d@gmail.com>
2024-10-21 09:45:30 +0200

    MENU A MEDIAS

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    //este un script de capa intermedia para que los gamemanager al cambiar de escena no se rayen, y los boyones puedan volver a funcionar de nuevoy asi no pierden la referencia, ya que no se van destruyendo.
+    public void ExitGame()
+    {
+        GameManager.instance.ExitGame();
+    }
+
+    public void LoadCharacter(int CharacterName)
+    {
+        GameManager.instance.characterType = (Characters)CharacterName;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        GameManager.instance.LoadScene(sceneName);
+    }
+
+}
