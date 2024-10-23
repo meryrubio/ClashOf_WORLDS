c3e9fbcb7123a7df3d822b6ef9116d5ff37440f7
meryrubio <mariarg9d@gmail.com>
2024-10-23 09:49:08 +0200

    texto habilidades

--- Assets/Scripts/ImageOption.cs
+++ Assets/Scripts/ImageOption.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public struct SelectionData
+{
+    public Sprite img;
+    public string textSkill;
+}
+
 public class ImageOption : MonoBehaviour
 {
-    public Sprite[] sprites;
+    public SelectionData[] data;
+
     private Image imageComponent;
+    public TMP_Text textComponent;
+    public GameObject skillsPanel; // Panel que contiene el texto de habilidades
 
     private void Start()
     {
@@ -14,6 +25,7 @@ public class ImageOption : MonoBehaviour
     }
     public void ChangeImage(int index)
     {
-        imageComponent.sprite = sprites[index];
+        imageComponent.sprite = data[index].img;
+        textComponent.text = data[index].textSkill;
     }
 }
