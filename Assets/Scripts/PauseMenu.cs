daf94ad364ae2678e4b7d185d087ab03becaf7ab
isdi12 <ismaelsesma11@gmail.com>
2025-05-20 17:53:20 +0200

    Menu de pausa acabado

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEngine.UI.GridLayoutGroup;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public bool pause = false;
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.instance.PauseMenu();
-        pauseMenu.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -18,7 +19,42 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pauseMenu.SetActive(true);
+            if (pause == false) 
+            { 
+                pauseMenu.SetActive(true);
+                pause = true;
+
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (pause == true)
+            {
+                Resume();
+            }
         }
     }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        pause = false;
+
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    public void Menu(string MenuName)
+    {
+        SceneManager.LoadScene(MenuName);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("EXIT!!");
+        Application.Quit();
+    }
+
 }
