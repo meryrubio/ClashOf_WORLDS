ac29a5587410a7886f30f2003c6012191d5305f1
isdi12 <ismaelsesma11@gmail.com>
2025-05-22 19:24:10 +0200

    powerups?????

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -10,9 +10,9 @@ namespace ClashofWorlds
         public Vector3 dir;
         private Rigidbody rb;
         private float currentTime = 0;
-
+        
         public AudioClip shootClip; //audio de disparo
-        public powerupBehaviour powerup;
+        
         // Start is called before the first frame update
         void Start()
         {
@@ -50,9 +50,32 @@ namespace ClashofWorlds
                 type.enemyType.health -= cm.character.damage;
                 gameObject.SetActive(false);
                 type.enemyType.Death(type.gameObject);
-                if(type.enemyType.health <= 0)
+                if (type.enemyType.health <= 0)
                 {
-                    
+                    if (Random.value <= 0.25f)
+                    {
+                        // 1) Elegir random el enum
+                        var allTypes = (powerupEnum[])System.Enum.GetValues(typeof(powerupEnum));
+                        powerupEnum chosen = allTypes[Random.Range(0, allTypes.Length)];
+
+                        // 2) Cargar prefab genérico desde Resources/Powerup/PowerupBehaviourPrefab
+                        GameObject prefab = Resources.Load<GameObject>("Powerup/PowerupBehaviourPrefab");
+                        if (prefab == null)
+                        {
+                            Debug.LogError("No encuentro Resources/Powerup/PowerupBehaviourPrefab");
+                        }
+                        else
+                        {
+                            // 3) Instanciarlo en la posición del enemigo
+                            GameObject pu = Instantiate(prefab, transform.position, Quaternion.identity);
+
+                            // 4) Asignar el tipo para que en Start() el script monte mesh y lógica
+                            var pb = pu.GetComponent<powerupBehaviour>();
+                            if (pb != null)
+                                pb.powerupType = chosen;
+                        }
+
+                    }
                 }
             }
         }
--- Assets/Scripts/Powerups/powerupBehaviour.cs
+++ Assets/Scripts/Powerups/powerupBehaviour.cs
@@ -36,4 +36,6 @@ public class powerupBehaviour : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    
 }
